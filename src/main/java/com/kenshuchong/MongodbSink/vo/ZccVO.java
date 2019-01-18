6d180f8acf14852534f62ddbca5f80cbdb5bdd6f
xuehenzcc <1255029471@qq.com>
2019-01-18 10:56:14 +0800

    first commit

---
+++ src/main/java/com/kenshuchong/MongodbSink/vo/ZccVO.java
@@ -0,0 +1,42 @@
+package com.kenshuchong.MongodbSink.vo;
+
+public class ZccVO {
+
+	private String name;
+	private String name2;
+	private String name3;
+	private String name4;
+	private String name5;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName2() {
+		return name2;
+	}
+	public void setName2(String name2) {
+		this.name2 = name2;
+	}
+	public String getName3() {
+		return name3;
+	}
+	public void setName3(String name3) {
+		this.name3 = name3;
+	}
+	public String getName4() {
+		return name4;
+	}
+	public void setName4(String name4) {
+		this.name4 = name4;
+	}
+	public String getName5() {
+		return name5;
+	}
+	public void setName5(String name5) {
+		this.name5 = name5;
+	}
+	
+	
+}
